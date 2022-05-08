04a17b62b746e9838b07bd6a36134ed55489a140
ahnafwarith <corp.ahnafwarid@gmail.com>
2022-05-08 21:50:34 +0600

    added fakeData styled site and lot more

--- src/App.js
+++ src/App.js
@@ -4,15 +4,19 @@ import Home from './Pages/Home/Home';
 import Login from './Pages/Login/Login/Login';
 import Register from './Pages/Login/Register/Register';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './Pages/Shared/Header/Header';
+import Footer from './Pages/Shared/Footer/Footer';
 
 function App() {
   return (
     <div>
+      <Header></Header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="login" element={<Login />} />
         <Route path="register" element={<Register />} />
       </Routes>
+      <Footer></Footer>
     </div>
   );
 }
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,10 +1,10 @@
-import { signOut } from 'firebase/auth';
 import React from 'react';
-import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
-import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
+import './Header.css'
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { signOut } from 'firebase/auth';
 import auth from '../../../firebase.init';
-import logo from '../../../images/logo.png'
+import { useAuthState } from 'react-firebase-hooks/auth';
 
 const Header = () => {
     const [user] = useAuthState(auth);
@@ -12,45 +12,33 @@ const Header = () => {
         signOut(auth)
     }
     return (
-        <>
-            <Navbar collapseOnSelect sticky='top' expand="lg" bg="dark" variant="dark">
+        <div>
+            <Navbar collapseOnSelect sticky='top' expand="lg">
                 <Container>
-                    <Navbar.Brand as={Link} to="/"><img height={40} src={logo} alt="" /></Navbar.Brand>
+                    <Navbar.Brand as={Link} to="/"><img height={40} src="" alt="" /></Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="me-auto">
-                            <Nav.Link href="home#services">Services</Nav.Link>
-                            <Nav.Link href="home#experts">Experts</Nav.Link>
-                            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                                <NavDropdown.Divider />
-                                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                            </NavDropdown>
+                            <Nav.Link href="home#services"><span className='link'>Services</span></Nav.Link>
+                            <Nav.Link href="home#expert"><span className='link'>Expert</span></Nav.Link>
                         </Nav>
                         <Nav>
-                            <Nav.Link as={Link} to="/about">About</Nav.Link>
-                            {
-                                user && <>
-                                    <Nav.Link as={Link} to="/adduser">Add</Nav.Link>
-                                    <Nav.Link as={Link} to="/manageservices">Manage</Nav.Link>
-                                    <Nav.Link as={Link} to="/orders">Orders</Nav.Link>
-                                </>
-                            }
                             {
                                 user ?
-                                    <button onClick={handleSignOut} className='btn btn-primary'>Signout</button>
+                                    <button onClick={handleSignOut} className='btn btn-success me-2 pb-2'>Signout</button>
                                     :
                                     <Nav.Link as={Link} to="/login">
-                                        Login
+                                        <span className='link'>Sign in</span>
                                     </Nav.Link>
                             }
+                            {/* <Nav.Link as={Link} to="/login"><span className='link'>Sign in</span></Nav.Link> */}
+                            <Nav.Link as={Link} to="/blogs"><span className='link'>Blogs</span></Nav.Link>
+                            <Nav.Link as={Link} to="/about"><span className='link'>About me</span></Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
-        </>
+        </div>
     );
 };
 
